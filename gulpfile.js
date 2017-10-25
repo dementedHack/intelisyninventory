c17a75466c725735746ace1cb6a9faea43c805fd
dementedHack <kaiperez@yahoo.com>
2017-10-24 22:43:45 -0700

    Initial Commit
    This is the initial commit

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+const gulp = require('gulp');
+
+// Logs message just to test out gulp
+
+gulp.task('message', function(){
+	return console.log('Gulp is running');
+})
---
+++ routes/asset_addition.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+// Firebase
+var firebase = require("firebase");
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAqVtjnVwoi5HhyWnsW7qLNSwPGYaZ-2TA",
+    authDomain: "intelisynassetmanagement.firebaseapp.com",
+    databaseURL: "https://intelisynassetmanagement.firebaseio.com",
+    projectId: "intelisynassetmanagement",
+    storageBucket: "intelisynassetmanagement.appspot.com",
+    messagingSenderId: "241325053027"
+  };
+  firebase.initializeApp(config);
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    console.log('User is logged in');
+  } else {
+    // No user is signed in.
+    console.log('User is not logged in')
+  }
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('asset_addition', { title: 'Express' });
+});
+
+module.exports = router;
